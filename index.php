84368468d1b4673a97fc738b59163745041f52a8
Antoine Brassard <antoine.brassard@gmail.com>
2018-04-05 20:23:46 -0400

    merge
    Signed-off-by: Antoine Brassard <antoine.brassard@gmail.com>

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require_once "libraries-communes-2018-03-28.php";
 require_once "classe-mysql-2018-03-18.php";
 require_once "sqlConnection.php";
 require_once "View.php";
+    require_once "Utilitaires/libraries-communes-2018-03-28.php";
+    require_once "Utilitaires/classe-mysql-2018-03-18.php";
 
 if (isset($_GET['controller']) && isset($_GET['action'])) {
     $controller = $_GET['controller'];
