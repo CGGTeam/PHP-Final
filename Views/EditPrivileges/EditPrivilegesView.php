b6e4514ffa70ce8cb7d5f7ff96ee5b7e657cd277
simon-boyer <si.boyer@hotmail.ca>
2018-04-09 01:51:12 -0400

    Privileges access

--- Views/EditPrivileges/EditPrivilegesView.php
+++ Views/EditPrivileges/EditPrivilegesView.php
@@ -1,1 +1,268 @@
-edit privileges
+<link href="Style/privilegeStyles.css" rel="stylesheet" type="text/css">
+<div class="container">
+    <h1>LES PRIVILÈGES D'ACCÈS</h1>
+    <div style="width: 90%; margin: auto">
+        <form>
+            <table border="1" cellspacing="5" cellpadding="5">
+                <tbody>
+                <tr>
+                    <th scope="col">Nom d'utilisateur / Cours-Session</th>
+                    <th scope="col">ZZZ-ZZZ
+                        <div class="sousTitre">A-2099</div></th>
+                    <th scope="col">ZZZ-ZZZ
+                        <div class="sousTitre">A-2099</div></th>
+                    <th scope="col">...</th>
+                    <th scope="col">ZZZ-ZZZ
+                        <div class="sousTitre">A-2099</div></th>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td>Nom, Prenom </td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td>Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                <tr>
+                    <td> Nom, Prenom</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                    <td>&nbsp;</td>
+                    <td><div class="checkbox">
+                            <label>
+                                <input name="checkbox" type="checkbox" id="checkbox">
+                                <em class="helper"></em> </label>
+                        </div></td>
+                </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
