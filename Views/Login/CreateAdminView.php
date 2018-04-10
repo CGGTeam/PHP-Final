428330b692a8e70bf53ef9a5166e972ad34905fb
Michael-John <michael.john.tech@gmail.com>
2018-04-09 21:34:06 -0400

    started adminpage modifications

--- Views/Login/CreateAdminView.php
+++ Views/Login/CreateAdminView.php
@@ -1,16 +1,17 @@
 <body>
 <?php var_dump($model) ?>
+<link href="/michael/michael/Views/Login/LoginStyle.css" rel="stylesheet" type="text/css">
 <div id="sCreerAdmin">
     <form id="frmCreerAdmin" method="post" action="index.php?controller=Login&action=CreerAdmin">
-        <label for="tbNomUtilisateur">Nom D'Utilisateur</label>
-        <input id="tbNomUtilisateur" name="tbNomUtilisateurAdmin" type="text"/><br/>
-        <label for="tbNomComplet">Nom Complet</label>
-        <input id="tbNomComplet" name="tbNomCompletAdmin" type="text"/><br/>
-        <label for="tbCourriel">Courriel</label>
-        <input id="tbCourriel" name="tbCourrielAdmin" type="text"/><br/>
-        <label for="tbMotDePasse">Mot de passe</label>
-        <input id="tbMotDePasse" name="tbMotDePasseAdmin" type="password"/><br/>
-        <input id="btnSoumettre" name="btnSoumettre" type="submit" value="Creer"/>
+        <table>
+            <input id="tbNomUtilisateur" name="tbNomUtilisateurAdmin" type="text" placeholder="Nom D'Utilisateur"/><
+            <input id="tbNomComplet" name="tbNomCompletAdmin" type="text" placeholder="Nom Complet"/>
+            <label for="tbCourriel">Courriel</label>
+            <input id="tbCourriel" name="tbCourrielAdmin" type="text"/>
+            <label for="tbMotDePasse">Mot de passe</label>
+            <input id="tbMotDePasse" name="tbMotDePasseAdmin" type="password"/>
+            <input id="btnSoumettre" name="btnSoumettre" type="submit" value="Creer"/>
+        </table>
     </form>
 </div>
 </body>
