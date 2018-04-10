44da1de51afcaa0ccf91dd3f209d262afaa72499
Michael-John <michael.john.tech@gmail.com>
2018-04-09 21:37:32 -0400

    created placeholders

--- Views/Login/CreerAdminView.php
+++ Views/Login/CreerAdminView.php
@@ -4,12 +4,10 @@
 <div id="sCreerAdmin">
     <form id="frmCreerAdmin" method="post" action="index.php?controller=Login&action=CreerAdmin">
         <table>
-            <input id="tbNomUtilisateur" name="tbNomUtilisateurAdmin" type="text" placeholder="Nom D'Utilisateur"/><
+            <input id="tbNomUtilisateur" name="tbNomUtilisateurAdmin" type="text" placeholder="Nom D'Utilisateur"/>
             <input id="tbNomComplet" name="tbNomCompletAdmin" type="text" placeholder="Nom Complet"/>
-            <label for="tbCourriel">Courriel</label>
-            <input id="tbCourriel" name="tbCourrielAdmin" type="text"/>
-            <label for="tbMotDePasse">Mot de passe</label>
-            <input id="tbMotDePasse" name="tbMotDePasseAdmin" type="password"/>
+            <input id="tbCourriel" name="tbCourrielAdmin" type="text" placeholder="Courriel"/>
+            <input id="tbMotDePasse" name="tbMotDePasseAdmin" type="password" placeholder="Mot de passe"/>
             <input id="btnSoumettre" name="btnSoumettre" type="submit" value="Creer"/>
         </table>
     </form>
