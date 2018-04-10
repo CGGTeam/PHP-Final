bc3334d0c53967b9d84d74b0fc1bc0291e0ec77b
Michael-John <michael.john.tech@gmail.com>
2018-04-09 21:32:54 -0400

    Login page css done

--- Views/header.php
+++ Views/header.php
@@ -4,5 +4,8 @@
     <head>
         <link rel="stylesheet" href="https://use.typekit.net/gij1vpi.css">
         <link href="Style/main.css" rel="stylesheet" type="text/css">
+
+        <!-- Adobe TypeKit fonts -->
+        <link rel="stylesheet" href="https://use.typekit.net/gij1vpi.css">
     </head>
     <body>
