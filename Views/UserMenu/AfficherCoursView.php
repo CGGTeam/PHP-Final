540702d92a67a7d88728f2d198684ecf884454d5
Antoine Brassard <antoine.brassard@gmail.com>
2018-04-12 21:32:01 -0400

    AfficherCoursView done

--- Views/UserMenu/AfficherCoursView.php
+++ Views/UserMenu/AfficherCoursView.php
@@ -1,1 +1,113 @@
-afficher cours
+<link href="Views/UserMenu/AfficherCoursStyle.css" rel="stylesheet" type="text/css">
+<form class="container">
+    <table border="1" cellspacing="5" cellpadding="5">
+        <tbody>
+        <tr>
+            <th scope="col">Numéro du document</th>
+            <th scope="col">Date du cours</th>
+            <th scope="col">Numéro de séquence</th>
+            <th scope="col">Catégorie</th>
+            <th scope="col">Titre</th>
+            <th scope="col">Description</th>
+            <th scope="col">Nombre de pages</th>
+            <th scope="col">Dernière mise à jour</th>
+            <th scope="col">Nombre de jours restants</th>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        <tr>
+            <td>###</td>
+            <td>AAAA-MM-JJ</td>
+            <td>###</td>
+            <td>CATÉGORIE</td>
+            <td><a>TITRE.ext</a></td>
+            <td>
+                <button>DESCRIPTION</button>
+            </td>
+            <td>###p.</td>
+            <td>AAAA-MM-JJ</td>
+            <td>### jours</td>
+        </tr>
+        </tbody>
+    </table>
+    <button type="button" name="button" id="button"
+            onclick="window.location='?controller=UserMenu&action=ChoixCours';">
+        Retour
+    </button>
+</form>
