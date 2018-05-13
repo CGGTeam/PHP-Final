e8bb6ced8b8d8987839db336feb5939ad0d63ca1
Antoine Brassard <antoine.brassard@gmail.com>
2018-05-12 21:41:31 -0400

    Added reason for validation failure for group controller

---
+++ Models/EditGroupes/EnumRaisons.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: antoi
+     * Date: 2018-05-12
+     * Time: 9:02 PM
+     */
+    
+    class EnumRaisons {
+        const ABSENT = 0;
+        const BD_COLLISION = 1;
+        const BD_ABSENT = 2;
+        const INVALIDE = 3;
+        const DOUBLON = 4;
+        const VALIDE = 5;
+    }
