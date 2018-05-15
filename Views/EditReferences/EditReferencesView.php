dc91c9252c6868ced24a7a0e889dd6a7b18a2c02
Antoine Brassard <antoine.brassard@gmail.com>
2018-05-15 19:46:53 -0400

    more improved css

--- Views/EditReferences/EditReferencesView.php
+++ Views/EditReferences/EditReferencesView.php
@@ -5,8 +5,10 @@
             <td>
                 <form method="post" action="?controller=EditReferences&action=EditListeSessions">
                     <button name="btnType" value="Session" type="submit">
-                        1. Sessions d'etude<br/>
-                        <i style="font-size: small">Cliquez sur le lien ci-dessus si vous désirez ajouter/modifier/retirer un ou plusieurs documents</i>
+                        <p>1. Sessions d'etude
+                        <p>
+                        <p class="desc">Cliquez sur le lien ci-dessus si vous désirez ajouter/modifier/retirer un ou
+                            plusieurs documents</p>
                     </button>
                 </form>
             </td>
@@ -15,8 +17,9 @@
             <td>
                 <form method="post" action="?controller=EditReferences&action=EditListeCours">
                     <button name="btnType" value="Cours" type="submit">
-                        2. Cours<br/>
-                        <i style="font-size: small">Cliquez sur le lien ci-dessus si vous désirez ajouter/modifier/retirer une ou plusieurs sessions, cours, catégories de documents et/ou utilisateurs</i>
+                        <p>2. Cours</p>
+                        <p class="desc">Cliquez sur le lien ci-dessus si vous désirez ajouter/modifier/retirer une ou
+                            plusieurs sessions, cours, catégories de documents et/ou utilisateurs</p>
                     </button>
                 </form>
             </td>
@@ -25,8 +28,9 @@
             <td>
                 <form method="post" action="?controller=EditReferences&action=EditListeCoursSessions">
                     <button name="btnType" value="CoursSession" type="submit">
-                        3. Cours-Session<br/>
-                        <i style="font-size: small">Cliquez sur le lien ci-dessus pour assigner les privilèges d'accès aux documents pour un ou plusieurs utilisateurs</i>
+                        <p>3. Cours-Session</p>
+                        <p class="desc">Cliquez sur le lien ci-dessus pour assigner les privilèges d'accès aux documents
+                            pour un ou plusieurs utilisateurs</p>
                     </button>
                 </form>
             </td>
@@ -35,8 +39,9 @@
             <td>
                 <form method="post" action="?controller=EditReferences&action=EditListeCategories">
                     <button name="btnType" value="Categorie" type="submit">
-                        4. Catégories<br>
-                        <i style="font-size: small">Cliquez sur le lien ci-dessus si vous désirez ajouter une série d'utilisateurs et les assigner à un cours-session existant</i>
+                        <p>4. Catégories</p>
+                        <p class="desc">Cliquez sur le lien ci-dessus si vous désirez ajouter une série d'utilisateurs
+                            et les assigner à un cours-session existant</p>
                     </button>
                 </form>
             </td>
@@ -45,8 +50,9 @@
             <td>
                 <form method="post" action="?controller=EditReferences&action=EditListeUtilisateurs">
                     <button name="btnType" value="Utilisateur" type="submit">
-                        5. Utilisateurs<br>
-                        <i style="font-size: small">Cliquez sur le lien ci-dessus si vous dériez effectuer du ménage dans la liste de docuements enregistrés</i>
+                        <p>5. Utilisateurs</p>
+                        <p class="desc">Cliquez sur le lien ci-dessus si vous désirez effectuer du ménage dans la liste
+                            de docuements enregistrés</p>
                     </button>
                 </form>
             </td>
@@ -55,8 +61,8 @@
             <td>
                 <form method="post" action="?controller=AdminMenu&action=AdminMenu">
                     <button type="submit">
-                        6. Retourner au menu principal<br/>
-                        <i style="font-size: small">Que dire de plus?</i>
+                        <p>6. Retourner au menu principal</p>
+                        <p class="desc">Que dire de plus?</p>
                     </button>
                 </form>
             </td>
