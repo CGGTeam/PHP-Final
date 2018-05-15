ec1c890d3a7f1f320c48ad56c1571fe69faad5ed
Michael-John <michael.john.tech@gmail.com>
2018-05-15 11:40:49 -0400

    fixed wrong action name and random ", true"

--- Views/EditDocuments/EditDocumentsView.php
+++ Views/EditDocuments/EditDocumentsView.php
@@ -68,7 +68,7 @@
         console.log(formData);
         if(!formData.values().next().done) {
             let xhr = new XMLHttpRequest();
-            xhr.open('POST', '?controller=EditDocuments&action=UploadDocuments, true', true);
+            xhr.open('POST', '?controller=EditDocuments&action=SauvegardeFichier', true);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     postChanges('Document', removeFirst)
